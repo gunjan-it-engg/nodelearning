b95f2698664505eaf7aa70cc8f2eb278a9ffd2b0
gunjan shrimali <gunjan1234@gmail.com>
2021-09-02 15:04:23 +0530

    fetching real-time weather update data from the api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,41 +1,70 @@
-// console.log('starting');
-// setTimeout(()=> {
-// 	console.log('20 seconds');
-// },20000)
+// // console.log('starting');
+// // setTimeout(()=> {
+// // 	console.log('20 seconds');
+// // },20000)
 
-// console.log('stopping');
+// // console.log('stopping');
+
+// var axios = require("axios").default;
+// // const request = require("request").default;
+
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/',
+//   params: {query: 'Stockholm'},
+//   headers: {
+//     'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+//     'x-rapidapi-key': 'c8baab0bc4mshd08d182ef228d04p14cff6jsn5c34904e5ae5'
+//   }
+// };
+ 
+//  const getData = (name)=>{
+
+
+// axios.request(options).then(function (response) {
+// 	const data = (response.data);
+//   console.log(data);
+//   const result = response.data.Places.filter(function(place) { 
+    
+//     // const a = placeId === 'STOC-sky';
+//     return place.PlaceName ===name;
+//   });
+//   // const result = response.data.forEach(Places => places);
+// 	console.log(result[0].PlaceName);
+//   // console.log(response.data.Places[0]);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+// }
+
+// getData("Stockholm")
+// // const url = "https://weatherapi-com.p.rapidapi.com/astronomy.json?q=india"
+// // request({ url : url },(error,response)=>{
+// // 	const data = JSON.parse(response.body) 
+// // 	console.log(data);
+// // })
+
 
 var axios = require("axios").default;
-// const request = require("request").default;
-
+var chalk = require('chalk');
 
 var options = {
   method: 'GET',
-  url: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/',
-  params: {query: 'Stockholm'},
+  url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  params: {q: '24.6036387,73.7098009'},
   headers: {
-    'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+    'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
     'x-rapidapi-key': 'c8baab0bc4mshd08d182ef228d04p14cff6jsn5c34904e5ae5'
   }
 };
 
 axios.request(options).then(function (response) {
-	const data = (response.data);
-  const result = response.data.Places.forEach(placeId === 'STOC-sky'=>{
-    console.log(placeId);
-    return placeId;
-  })
-  // const result = response.data.forEach(Places => places);
-	console.log(result);
-  // console.log(response.data.Places[0]);
+  console.log(chalk.green.inverse.bold("Wel come to real-time weather forcasting system"));
+  //console.log(response.data.current);   // showing the current weather of the city
+  console.log(chalk.inverse("Last Weather update", response.data.current.last_updated));
+  console.log(chalk.red.inverse.bold("current Temprature",+ response.data.current.temp_c + " c in degree"));
 }).catch(function (error) {
-	console.error(error);
+  console.error(error);
 });
 
-
-
-// const url = "https://weatherapi-com.p.rapidapi.com/astronomy.json?q=india"
-// request({ url : url },(error,response)=>{
-// 	const data = JSON.parse(response.body) 
-// 	console.log(data);
-// })
