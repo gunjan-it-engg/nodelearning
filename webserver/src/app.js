7cfa221a1ca432a14f26453ac658390fb868a195
gunjan shrimali <gunjan.it.engg@gmail.com>
2021-09-08 04:42:51 -0700

    Advance Teamplating with partial header and footer

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,23 +1,30 @@
+// for better way to handel the request me can use header partial
+
 const express = require("express");
 const path = require("path");
 const { help } = require("yargs");
+const hbs = require("hbs");
 
 const app = express();
 
 console.log(__dirname);
 console.log(__filename);
 
-const publicDirectoryPath = path.join(__dirname, "../public");
+// for header partial we are using the advance templating.
+const partialsPath = path.join(__dirname, "../templating/partial");
+
+// const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templating/views");
 // app.use(express.static(publicDirectoryPath));
 app.set("view engine", "hbs");
-// app.use(express.static(publicDirectoryPath));
-
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 app.get("/", (req, res) => {
   res.render("index", { title: "home from hbs", name: "server" });
 });
 
 app.get("/help", (req, res) => {
-  res.render("help");
+  res.render("help", { title: "about|maker" });
 });
 
 // this will be used for sending json formatting data in the front end.
